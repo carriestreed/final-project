3594c392c751b995558e484d381d2964aeef2d6c
Carrie Streed <cmstreed@gmail.com>
2016-05-01 18:23:31 -0400

    adding a login screen

---
+++ Wander/App/Screens/LoginScreen.js
@@ -0,0 +1,23 @@
+import React, {
+  View,
+  Text,
+  Image,
+  Component,
+  StyleSheet,
+  TextInput,
+  TouchableHighlight,
+  ActivityIndicatorIOS,
+} from 'react-native';
+
+class LoginScreen extends Component {
+  render(){
+    return (
+      <View>
+        <Text>Hi, I'm the login screen</Text>
+      </View>
+    )
+  }
+}
+
+
+export default LoginScreen;
--- Wander/index.ios.js
+++ Wander/index.ios.js
@@ -1,28 +1,21 @@
 import React, {
   AppRegistry,
-  StyleSheet,
   Text,
   View,
   Component,
   NavigatorIOS
 } from 'react-native';
 
-import Main from './App/Components/Main';
 import StatusBarBg from './App/Components/StatusBarBg';
+import LoginScreen from './App/Screens/LoginScreen';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'transparent',
-  },
-});
 
 class Wander extends Component {
   render() {
     return (
       <View>
         <StatusBarBg />
-        <Text>Hello</Text>
+        <LoginScreen />
       </View>
     );
   }
