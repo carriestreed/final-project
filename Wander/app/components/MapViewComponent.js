6b54ef88a63916198e8df305a7c5742a84bb8ef4
Carrie Streed <cmstreed@gmail.com>
2016-05-04 09:51:24 -0400

    adjusted map height. if it spans the height of the screen, unable to scroll back up... it seems...

--- Wander/app/components/MapViewComponent.js
+++ Wander/app/components/MapViewComponent.js
@@ -12,7 +12,7 @@ class MapViewComponent extends Component{
   render(){
     return(
       <MapView
-        style = { { height: 700, width: 414 } }
+        style = { { height: 600, width: 414 } }
         initialRegion={{
           latitude: 33.9628,
           longitude: 18.4098,
