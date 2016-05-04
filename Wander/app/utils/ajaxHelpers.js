d233b9f0aa23bda28a807ecec92203cb0154490c
Carrie Streed <cmstreed@gmail.com>
2016-05-03 21:34:50 -0400

    minor adj to api endpoint

--- Wander/app/utils/ajaxHelpers.js
+++ Wander/app/utils/ajaxHelpers.js
@@ -5,7 +5,7 @@ import API_KEY from './keys.js'
 const ajaxHelpers = {
 
   callCountry: function(userInput){
-    return fetch('https://api.flickr.com/services/rest/?format=json&nojsoncallback=1&method=flickr.photos.search&api_key=' + API_KEY + '&sort=interestingness-desc&group_id=41425956%40N00&tags=' + userInput, {'method': 'GET'})
+    return fetch('https://api.flickr.com/services/rest/?format=json&nojsoncallback=1&method=flickr.photos.search&api_key=' + API_KEY + '&sort=interestingness-desc&group_id=13197975%40N00&has_geo=&tags=' + userInput, {'method': 'GET'})
   }
 }
 
